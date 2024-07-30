8661b0fc409c9434f2150cd297e974df01aa475d
rydfadli <rydfadli@gmail.com>
2024-07-30 07:28:31 +0000

    modified:   app/components/items/stats.js

--- app/components/items/stats.js
+++ app/components/items/stats.js
@@ -22,8 +22,11 @@ const Stats = ({number, persentage = '', text}) => {
     <div className="md:w-1/4 w-full md:text-start text-center text-sm md:text-md mb-7 md:mb-0">
 
       <motion.h1 className="text-7xl md:mb-5 mb-2 text-center mx-auto">
-        {rounded}<span>{persentage}</span>
+        {rounded}
       </motion.h1>
+      <p className='text-3xl'>{
+        persentage
+      }</p>
       
       <p className="max-w-[200px] mx-auto">{text}</p>
     </div>
