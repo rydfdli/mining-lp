44df59b5ef40a90ac64b2fd6b6f627efcc638b14
rydfdli <rydfadli@gmail.com>
2024-08-03 01:46:29 +0800

    Changes to be committed: 	modified:   app/layout.js
    Changes not staged for commit:
    	modified:   app/components/contact.js
    	modified:   app/components/navbar.js
    
    Untracked files:
    	app/about/
    	app/contact/
    	app/services/

--- app/layout.js
+++ app/layout.js
@@ -16,7 +16,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={poppins.className}>
         <Navbar />
-          {children}
+            {children}
         <Footer />
       </body>
     </html>
